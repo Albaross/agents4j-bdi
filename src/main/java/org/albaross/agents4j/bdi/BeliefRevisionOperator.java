d39173b07620b83596906583b361af3266d63d4d
Barbi <Manuel.Barbi@adesso.de>
2017-07-29 16:49:03 +0200

    adapted core changes and increased version

--- src/main/java/org/albaross/agents4j/bdi/BeliefRevisionOperator.java
+++ src/main/java/org/albaross/agents4j/bdi/BeliefRevisionOperator.java
@@ -3,23 +3,24 @@ package org.albaross.agents4j.bdi;
 import java.util.Set;
 
 import org.albaross.agents4j.core.common.OperationParam;
-import org.albaross.agents4j.core.common.PerceptionOperator;
+import org.albaross.agents4j.core.common.Operator;
 
 /**
  * 
  * @author Manuel Barbi
  *
  */
-public interface BeliefRevisionOperator<P, B> extends PerceptionOperator<P> {
+public interface BeliefRevisionOperator<P, A, B> extends Operator<P, A> {
 
 	@Override
-	default <A> void perceive(OperationParam<P, A> param, P perception) {
+	default void process(OperationParam<P, A> param) {
+		P perception = param.getPerception();
 		Set<B> statements = translate(perception);
 		revision(param, statements);
 	}
 
 	Set<B> translate(P perception);
 
-	<A> void revision(OperationParam<P, A> param, Set<B> statements);
+	void revision(OperationParam<P, A> param, Set<B> statements);
 
 }
